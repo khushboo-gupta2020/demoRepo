ad8ce91400d0fc199bff389335e51da44acf1a9d
khusboo Gupta <kgupta003c@cable.comcast.com>
2020-02-27 02:21:44 -0500

    My first project commit in github

---
+++ src/main/java/com/qa/util/UtilPage.java
@@ -0,0 +1,9 @@
+package com.qa.util;
+
+public class UtilPage {
+
+	public void util()
+	{
+		System.out.println("UtilPage");
+	}
+}
---
+++ src/test/java/com/qa/tests/TestPage.java
@@ -0,0 +1,12 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class TestPage {
+
+	@Test
+	public void testpage()
+	{
+		System.out.println("testpage");
+	}
+}
